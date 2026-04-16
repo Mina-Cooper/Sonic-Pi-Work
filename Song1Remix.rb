fae2af5b1429446aae7594be1abb2afda9e0ddcd
Mina-Cooper <cooperminaf@gmail.com>
2026-04-16 11:26:02 -0700

    Update Song1Remix.rb

--- Song1Remix.rb
+++ Song1Remix.rb
@@ -1,30 +1,46 @@
-# Welcome to Sonic Pi
+#heart
 
 #variables
 heartintro = "C:/Users/Mina_Cooper/Downloads/heartintro.wav"
 r1end = "C:/Users/Mina_Cooper/Downloads/end.wav"
 
 use_synth :pretty_bell
-a = 1
+use_bpm 90
 
-notes = [:cs4, :bs4, :cs4]
+notesone = [:es2, :e5, :c5]
+notestwo = [:g5, :f5]
+notesthree = [:f5, :e5, :d5, :e5]
 n = 0
 
 define :bgnotes do
-  play notes[n], amp: a
-  sleep 2
+  3.times do
+    play notesone[n]
+    n = n + 1
+  end
+  n = 0
+  2.times do
+    play notestwo[n]
+    sleep 1
+    n = n + 1
+  end
+  n = 0
+  play :f5, sustain: 1
+  4.times do
+    play notesthree[n]
+    sleep 1
+    n = n + 1
+  end
+  n = 0
 end
 
-live_loop :thirdrow do
-  5.times do
-    5.times do
-      bgnotes
-    end
-    a = a - 0.25
-    n = n + 1
+live_loop :intro do
+  3.times do
+    bgnotes
   end
   stop
 end
+
+sleep 15
 
 play :b4, sustain: 2, amp: 0.5
 sleep 1
@@ -38,10 +54,7 @@ play :e4, sustain: 2, amp: 0.7
 sleep 2
 
 sample heartintro
-sleep 4
-
-use_bpm 96
-use_synth :pretty_bell
+sleep 5
 
 live_loop :firstrow do
   3.times do
